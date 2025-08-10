911d5ad8d1de70f3bd5eb5bd80241253ec4e112e
anotherbondan <anandagautama3112@gmail.com>
2025-08-10 20:17:17 +0700

    fix: next link

--- src/modules/SyariahChatbotModule/components/sidebar.tsx
+++ src/modules/SyariahChatbotModule/components/sidebar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useSession } from "@/hooks/useSession";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Sidebar() {
@@ -28,15 +29,15 @@ export default function Sidebar() {
     <aside className="w-64 min-h-full px-4 py-8 flex flex-col bg-white transition-all duration-300 ease-in-out relative z-10 shadow-xl">
       <nav className="flex-grow overflow-y-auto space-y-9">
         {chatTitles.map((text, idx) => (
-          <a
+          <Link
             key={idx}
-            href="/chatbot/"
+            href="/chatbot"
             className="flex w-full h-8 items-center justify-center p-[1px] rounded-lg bg-gradient-primary mb-3"
           >
             <div className="flex w-full h-full items-center justify-center bg-white hover:bg-gray-100 px-3 rounded-lg">
               <h1 className="text-center">{text}</h1>
             </div>
-          </a>
+          </Link>
         ))}
       </nav>
       <button className="flex items-center justify-center w-full bg-[#72d3bd] active:bg-[#1a6e6a] rounded-lg px-3 py-2 mb-4 transition-colors">
