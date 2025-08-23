bfb5f6469c4129f65425ac90ea58e7516820de32
anotherbondan <anandagautama3112@gmail.com>
2025-08-23 11:41:39 +0700

    add slash

--- src/modules/SyariahChatbotModule/index.tsx
+++ src/modules/SyariahChatbotModule/index.tsx
@@ -28,7 +28,7 @@ export default function ChatbotModule() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/chatbot`,
+        `${process.env.NEXT_PUBLIC_API_URL}/chatbot/`,
         {
           method: "POST",
           headers: {
